413b2ae8d00a86730acf2fd43923144b39e43633
Кирилл <kiryamiass7@gmail.com>
2023-02-12 22:55:30 +0500

    29 урок середина2

--- blog/userapp/urls.py
+++ blog/userapp/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from userapp import views
+from django.contrib.auth.views import LogoutView
 
 app_name = 'userapp'
 
 urlpatterns = [
     path('login/', views.UserLoginView.as_view(), name='login'),
+    path('/logout',views.LogoutView.as_view(),name='logout'),
+    path('/register',views.UserCreateView.as_view(),name='register')
 
 
 ]
--- blog/userapp/views.py
+++ blog/userapp/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
-from django.contrib.auth.views import LoginView,LogoutView
+from django.contrib.auth.views import LoginView, LogoutView
+from .forms import RegistrationForm
+from django.views.generic import CreateView
+from .models import BlogUser
+from django.urls import reverse_lazy
 
 
 class UserLoginView(LoginView):
     template_name = 'userapp/login.html'
+
+
+class UserCreateView(CreateView):
+    model = BlogUser
+    template_name = 'userapp/register.html'
+    form_class = RegistrationForm
+    success_url = reverse_lazy('userapp:login')
+
 
 # Create your views here.
